c371b2b8350d988bc993c10b79a290a76f069a33
alexandr <shuto4ka1@gmail.com>
2020-03-05 22:39:43 +0300

    Add refresh scope watcher test.

---
+++ src/test/java/ru/spb/devclub/flexscheduler/configuration/RefreshScopeWatcherConfigurationTest.java
@@ -0,0 +1,19 @@
+package ru.spb.devclub.flexscheduler.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.FilteredClassLoader;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
+import ru.spb.devclub.flexscheduler.watcher.RefreshScopeWatcher;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+class RefreshScopeWatcherConfigurationTest {
+
+    @Test
+    public void whenDependentClassIsNotPresent_thenBeanMissing() {
+        new ApplicationContextRunner().withUserConfiguration(RefreshScopeWatcherConfiguration.class)
+                .withClassLoader(new FilteredClassLoader(RefreshScopeRefreshedEvent.class))
+                .run(context -> assertThat(context).doesNotHaveBean(RefreshScopeWatcher.class));
+    }
+}
