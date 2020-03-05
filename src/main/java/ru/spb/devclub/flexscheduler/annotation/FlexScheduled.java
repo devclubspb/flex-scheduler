99a7816417856c34a0c11461ae3a4a2818c7f4ed
Grig Alex <alexengrigdev@gmail.com>
2020-03-05 21:03:07 +0300

    Add FlexScheduled annotation

---
+++ src/main/java/ru/spb/devclub/flexscheduler/annotation/FlexScheduled.java
@@ -0,0 +1,16 @@
+package ru.spb.devclub.flexscheduler.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Grig Alex
+ * @see org.springframework.scheduling.annotation.Scheduled
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FlexScheduled {
+    String value() default "Default value";
+}
