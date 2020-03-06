e05c337ed9136c7cfa3ce2a45ef0a217ab83fd8d
Grig Alex <alexengrigdev@gmail.com>
2020-03-06 21:06:28 +0300

    Add registries to FlexScheduledAnnotationBeanPostProcessor

--- src/main/java/ru/spb/devclub/flexscheduler/annotation/FlexScheduledAnnotationBeanPostProcessor.java
+++ src/main/java/ru/spb/devclub/flexscheduler/annotation/FlexScheduledAnnotationBeanPostProcessor.java
@@ -9,7 +9,6 @@ import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.util.Assert;
 import org.springframework.util.ReflectionUtils;
-import ru.spb.devclub.flexscheduler.ConcurrentTaskRegistry;
 import ru.spb.devclub.flexscheduler.Task;
 import ru.spb.devclub.flexscheduler.TaskRegistry;
 import ru.spb.devclub.flexscheduler.supplier.TriggerSupplier;
@@ -34,50 +33,63 @@ public class FlexScheduledAnnotationBeanPostProcessor implements BeanPostProcess
         return !methods.isEmpty() ? methods : null;
     };
 
-    private final Set<Class<?>> nonAnnotatedClasses = Collections.newSetFromMap(new ConcurrentHashMap<>(64));
-    private final TaskRegistry taskRegistry = getTaskRegistry();
+    private final Map<String, TaskRegistry> taskRegistries;
+    private final Set<Class<?>> nonAnnotatedClasses;
 
-    protected TaskRegistry getTaskRegistry() {
-        return new ConcurrentTaskRegistry();
+    public FlexScheduledAnnotationBeanPostProcessor(Map<String, TaskRegistry> taskRegistries) {
+        this.taskRegistries = taskRegistries;
+        this.nonAnnotatedClasses = Collections.newSetFromMap(new ConcurrentHashMap<>(64));
     }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         final Class<?> targetClass = AopProxyUtils.ultimateTargetClass(bean);
         if (AnnotationUtils.isCandidateClass(targetClass, FlexScheduled.class)) {
-            final Map<Method, Set<FlexScheduled>> annotatedMethods = getAnnotatedMethods(targetClass);
-            if (annotatedMethods.isEmpty()) {
+            final Map<Method, Set<FlexScheduled>> methodAnnotations = getMethodsWithFlexScheduledAnnotation(targetClass);
+            if (methodAnnotations.isEmpty()) {
                 this.nonAnnotatedClasses.add(targetClass);
             } else {
-                annotatedMethods.forEach((method, flexScheduledMethods) -> flexScheduledMethods.forEach(flexScheduled -> {
-                    processFlexScheduled(flexScheduled, method, bean);
-                }));
+                for (Map.Entry<Method, Set<FlexScheduled>> entry : methodAnnotations.entrySet()) {
+                    final Method method = entry.getKey();
+                    final Set<FlexScheduled> annotations = entry.getValue();
+                    annotations.forEach(annotation -> processFlexScheduled(annotation, method, bean));
+                }
             }
         }
         return bean;
     }
 
-    private Map<Method, Set<FlexScheduled>> getAnnotatedMethods(Class<?> targetType) {
+    private Map<Method, Set<FlexScheduled>> getMethodsWithFlexScheduledAnnotation(Class<?> targetType) {
         return MethodIntrospector.selectMethods(targetType, SELECTOR);
     }
 
     private void processFlexScheduled(FlexScheduled annotation, Method method, Object bean) {
+        final Task task = createTask(annotation, method, bean);
+        final String registryName = annotation.registry();
+        final TaskRegistry taskRegistry = taskRegistries.get(registryName);
+        try {
+            taskRegistry.schedule(task, false);
+        } catch (Exception e) {
+            e.printStackTrace(); //TODO: Handle exceptions
+        }
+    }
+
+    private Task createTask(FlexScheduled annotation, Method method, Object bean) {
         final String taskName = createTaskName(annotation, method, bean);
         final TriggerSupplier triggerSupplier = createTriggerSupplier(annotation, method, bean);
         final Runnable runnable = createRunnable(annotation, method, bean);
-        final Task task = new Task(taskName, runnable, triggerSupplier);
-        taskRegistry.schedule(task, false);
+        return new Task(taskName, runnable, triggerSupplier);
     }
 
-    protected String createTaskName(FlexScheduled annotation, Method method, Object bean) {
+    private String createTaskName(FlexScheduled annotation, Method method, Object bean) {
         return method.getClass().getName() + "#" + method.getName();
     }
 
-    protected TriggerSupplier createTriggerSupplier(FlexScheduled annotation, Method method, Object bean) {
+    private TriggerSupplier createTriggerSupplier(FlexScheduled annotation, Method method, Object bean) {
         return () -> new DisposableTrigger(annotation.fixedDelay(), TimeUnit.MILLISECONDS);
     }
 
-    protected Runnable createRunnable(FlexScheduled annotation, Method method, Object bean) {
+    private Runnable createRunnable(FlexScheduled annotation, Method method, Object bean) {
         Assert.isTrue(method.getParameterCount() == 0, "Only no-arg methods may be annotated with @FlexScheduled");
         Method invocableMethod = AopUtils.selectInvocableMethod(method, bean.getClass());
         return () -> {
